5943c3b9bc0c230b0650bd8b16911a1f83809f24
jc487 <jc487@ntrs.com>
2020-04-18 15:31:39 -0500

    Updates

---
+++ precache-manifest.247021227ab12f92ed6928f5cced8d9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f867d667bbeca5e6aabc2e6bc8f09fd",
+    "url": "/dsc468finalproject/index.html"
+  },
+  {
+    "revision": "b6cfe1f4a687aafc1538",
+    "url": "/dsc468finalproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36fb853b09e8c9b74812",
+    "url": "/dsc468finalproject/static/js/2.be7e122f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dsc468finalproject/static/js/2.be7e122f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cfe1f4a687aafc1538",
+    "url": "/dsc468finalproject/static/js/main.c17c278e.chunk.js"
+  },
+  {
+    "revision": "1be33dcf134df2eaf9c5",
+    "url": "/dsc468finalproject/static/js/runtime-main.8379a499.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dsc468finalproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
